ee6b7bb2c532d925b8053fafd0d1a7bc51af6c53
Masaharu Suizu <masaharu.sui@gmail.com>
2025-12-15 18:37:47 +0900

    Commit main.py.

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-def main():
-    print("Hello from update-github-notifications-status!")
+import os
+import requests
+
+def set_request_headers(token: str) -> dict:
+    return {
+        "Authorization": f"token {token}",
+        "Accept": "application/vnd.github+json",
+    }
+
+
+def call_github_api_by_get(url: str, token: str, timeout: int) -> requests.Response:
+    headers  = set_request_headers(token)
+    response = requests.get(url, headers=headers, timeout=timeout)
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"GitHub API Error: GET:{url} - {response.status_code} - {response.text}"
+        )
+    return response
+
+
+def call_github_api_by_patch(url: str, token: str, timeout: int) -> requests.Response:
+    headers  = set_request_headers(token)
+    response = requests.patch(url, headers=headers, timeout=timeout)
+    if response.status_code != 205:
+        raise RuntimeError(
+            f"GitHub API Error: PATCH:{url} - {response.status_code} - {response.text}"
+        )
+    return response
 
 
 if __name__ == "__main__":
-    main()
+
+    token = os.getenv("GITHUB_TOKEN")
+    if not token:
+        raise ValueError("Failed to get ENV.")
+
+    url      = "https://api.github.com/notifications"
+    response = call_github_api_by_get(url, token, 10)
+    notifications = response.json()
+
+    for notification in notifications:
+        reason    = notification.get("reason")
+        is_unread = notification.get("unread")
+        title     = notification.get("subject", {}).get("title")
+        url       = notification.get("url")
+
+        if reason != "ci_activity" or not is_unread or not "succeeded" in title:
+            continue
+
+        call_github_api_by_patch(url, token, 10)
+
+        print(f"✅ Marked as read: {url}")
